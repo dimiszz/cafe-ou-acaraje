979fa43a2dbfa28cf3f1f550a28c51924e73d762
JolidPnake <rodrigo0607suguimoto@gmail.com>
2024-06-28 16:48:33 -0300

    feat: correção dos gráficos

--- rods/rods.py
+++ rods/rods.py
@@ -7,12 +7,13 @@ from collections import Counter
 from matplotlib import pyplot as plt
 
 quantidade_dados = 50
+quantidade_jogadas = 100
 dados = []
 for i in range(quantidade_dados):
     dados.append(random_dice())
 
 dado_escolhido = choice(dados)
-jogadas = play_dice(100, dado_escolhido)
+jogadas = play_dice(quantidade_jogadas, dado_escolhido)
 jogadas_unicas = Counter(jogadas)
 
 #print("########## JOGADAS ##########")
@@ -59,34 +60,38 @@ print_info(end - start, likelihoods[max_index], max_index, dados[max_index])
 print("########## GRÁFICO ##########")
 passo = 2
 quantidade_passos = 100
+quantidade_testes = 50
 quantidade_dados = 20
 
 plot_y = np.zeros(quantidade_passos)
 plot_x = np.zeros(quantidade_passos)
 
-for k in range(1, quantidade_passos+1):
+for k in range(quantidade_passos):
     acertos = 0
-    for i in range(50):
+
+    for i in range(quantidade_testes):
         dados = []
-        for i in range(quantidade_dados):
+
+        for j in range(quantidade_dados):
             dados.append(random_dice())
         
         dado_escolhido = choice(dados)
-        jogadas = play_dice(passo*k, dado_escolhido)
+        jogadas = play_dice(passo*(k+1), dado_escolhido)
         jogadas_unicas = Counter(jogadas)
 
-        likelihoods = likelihoods_c(dados, jogadas_unicas)
-        max_index = np.argmax(likelihoods)
+        loglikelihoods = loglikelihoods_c(dados, jogadas_unicas)
+        max_index = np.argmax(loglikelihoods)
 
         if (dados[max_index] == dado_escolhido).all():
             acertos += 1
-    plot_y[k-1] = acertos/50
-    plot_x[k-1] = passo*k
+        
+    plot_y[k] = acertos/quantidade_testes
+    plot_x[k] = passo*k
 
     barrinha(k, quantidade_passos)
 
-plt.title(f"Passo {passo}, Quantidade de passos{quantidade_passos}")
-plt.xlabel("quantidade de jogadas")
+plt.title(f"Passo: {passo}, Quantidade de passos: {quantidade_passos}\nQuantidade de testes: {quantidade_testes}, Quantidade de dados: {quantidade_dados}")
+plt.xlabel("Quantidade de jogadas")
 plt.ylabel("Frequencia de acertos")
 plt.plot(plot_x,plot_y)
 plt.show()
